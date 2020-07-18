388c9de259371ef0b3b0d0dd8eaf173d5871fa8f
Edem Reshidov <newfaceof@yandex.ru>
2020-07-18 19:42:48 +0300

    Переписывает App обратно в функциональный компонент, подключает redux hooks

--- src/App.js
+++ src/App.js
@@ -1,45 +1,50 @@
-import React from "react";
+import React, {useEffect} from "react";
 import "./scss/app.scss";
 
-import { Route } from "react-router-dom";
-import { Header } from "./components";
-import { Home, Cart } from "./pages";
-import { connect } from "react-redux";
+import {Route} from "react-router-dom";
+import {Header} from "./components";
+import {Home, Cart} from "./pages";
+import {useSelector, useDispatch} from "react-redux";
 import setPizzas from "./redux/actions/pizzas";
 
 const mapStateToProps = (state) => {
   return {
     pizzas: state.pizzas.items,
+    filters: state.filters,
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
     setPizzas: (items) => dispatch(setPizzas(items)),
-    dispatch  
-  }
-}
+    dispatch,
+  };
+};
 
-class App extends React.Component {
-  componentDidMount() {
+const App = (props) => {
+
+  const dispatch = useDispatch()
+  useEffect(() => {
     fetch("http://localhost:3000/db.json")
       .then((res) => res.json())
       .then((db) => {
-        this.props.setPizzas(db.pizzas)
+        dispatch(setPizzas(db.pizzas));
       });
-  }
+  }, []);
 
-  render() {
-    return (
-        <div className="wrapper">
-          <Header />
-          <div className="content">
-            <Route path="/" render={() => <Home pizzas={this.props.pizzas} />} exact />
-            <Route path="/cart" component={Cart} exact />
-          </div>
-        </div>
-    );
-  }
-}
+  return (
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <Route
+          path="/"
+          render={() => <Home pizzas={props.pizzas} />}
+          exact
+        />
+        <Route path="/cart" component={Cart} exact />
+      </div>
+    </div>
+  );
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
