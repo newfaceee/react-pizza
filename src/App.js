12790880a5302a4126cd1f8ca2fa95a56533c643
Edem <50662754+newfaceee@users.noreply.github.com>
2020-07-27 12:49:18 +0300

    Переносит получение пицц из App в Home

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
-import React, { useEffect } from "react";
-import axios from "axios";
+import React from "react";
 import "./scss/app.scss";
 
 import { Route } from "react-router-dom";
 import { Header } from "./components";
 import { Home, Cart } from "./pages";
-import { useDispatch } from "react-redux";
-import { fetchPizzas } from "./redux/actions/pizzas";
 
-const App = (props) => {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(fetchPizzas());
-  }, []);
+const App = () => {
   return (
     <div className="wrapper">
       <Header />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,25 @@
-import React, {useCallback} from "react";
-import {useSelector, useDispatch} from 'react-redux'
-import {setCategory} from '../redux/actions/filters';
+import React, { useCallback, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { setCategory } from "../redux/actions/filters";
 
-import {Categories, SortPopup, Pizza} from "../components";
-import {CATEGORIES, SORT_ITEMS} from '../constants'
+import { Categories, SortPopup, Pizza } from "../components";
+import { CATEGORIES, SORT_ITEMS } from "../constants";
+import { fetchPizzas } from "../redux/actions/pizzas";
 
 const Home = () => {
   const dispatch = useDispatch();
-  const items = useSelector(({pizzas}) => pizzas.items);
+
+  useEffect(() => {
+    dispatch(fetchPizzas());
+  }, []);
+
+  const items = useSelector(({ pizzas }) => {
+    return pizzas.items;
+  });
 
   const onSelectCategory = useCallback((index) => {
     dispatch(setCategory(index));
-  }, []); 
+  }, []);
 
   return (
     <div className="container">
