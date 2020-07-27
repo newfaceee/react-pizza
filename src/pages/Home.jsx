825cbb62fc9b9af871afdbef66d9e666ae18a3cc
Edem <50662754+newfaceee@users.noreply.github.com>
2020-07-27 17:37:03 +0300

    Добавляет функцию для обработки изменения сортировки в Redux

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,9 @@
 import React, { useCallback, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { setCategory } from "../redux/actions/filters";
+import { setCategory, setSortBy } from "../redux/actions/filters";
 
-import { Categories, SortPopup, Pizza } from "../components";
+import { Categories, SortPopup, Pizza, PizzaLoadingBlock } from "../components";
+
 import { CATEGORIES, SORT_ITEMS } from "../constants";
 import { fetchPizzas } from "../redux/actions/pizzas";
 
@@ -13,24 +14,28 @@ const Home = () => {
     dispatch(fetchPizzas());
   }, []);
 
-  const [items, isLoaded] = useSelector(({ pizzas }) => [
-    pizzas.items,
-    pizzas.isLoaded,
-  ]);
+  const { items, isLoaded } = useSelector(({ pizzas }) => pizzas);
+  const { category, sortBy } = useSelector(({ filters }) => filters);
 
   const onSelectCategory = useCallback((index) => {
     dispatch(setCategory(index));
   }, []);
 
+  const onSelectSortType = useCallback((index) => {
+    dispatch(setSortBy(SORT_ITEMS[index].type));
+  });
+
   return (
     <div className="container">
       <div className="content__top">
         <Categories onClickItem={onSelectCategory} items={CATEGORIES} />
-        <SortPopup items={SORT_ITEMS} />
+        <SortPopup onClickItem={onSelectSortType} items={SORT_ITEMS} />
       </div>
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">
-        {isLoaded && items.map((pizza) => <Pizza pizza={pizza} />)}
+        {isLoaded
+          ? items.map((pizza) => <Pizza pizza={pizza} />)
+          : Array(12).fill(<PizzaLoadingBlock />)}
       </div>
     </div>
   );
