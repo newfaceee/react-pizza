af52a1076450cbdb2ac87935eb11028c50a367d1
Edem <50662754+newfaceee@users.noreply.github.com>
2020-07-31 01:46:32 +0300

    Рефакторит способ хранения пицц
    - Добавляет вспомогательные функции
    - Выносит повторяющейся код
    - Добавляет обработку удаления определенной пиццы

--- src/redux/reducers/cart.js
+++ src/redux/reducers/cart.js
@@ -1,52 +1,81 @@
 const initialState = {
-  items: {},
+  items: [],
   totalPrice: 0,
   totalCount: 0,
 };
 
-const updateItems = (newItems, oldItems) => {
-  const { id } = newItems;
-  if (oldItems[id]) {
-  }
+const updateState = (newItems) => {
+  const totalCount = newItems.reduce(
+    (acc, currPizza) => acc + currPizza.count,
+    0
+  );
+
+  const totalPrice = newItems.reduce(
+    (acc, currPizza) => acc + currPizza.price * currPizza.count,
+    0
+  );
+  return [totalCount, totalPrice];
 };
 
 const cart = (state = initialState, action) => {
+  let newItems = [...state.items];
+  const index = newItems.findIndex((pizza) => {
+    return (
+      pizza.id === action.payload.id &&
+      pizza.doughType === action.payload.doughType &&
+      pizza.size === action.payload.size
+    );
+  });
   switch (action.type) {
-    case 'ADD_PIZZA_CART':
-      let newItems = {};
-
-      const index =
-        state.items[action.payload.id] &&
-        state.items[action.payload.id].findIndex((pizza) => {
-          return (
-            pizza.type === action.payload.type &&
-            pizza.size === action.payload.size
-          );
-        });
-      if (index !== 'undefined' && index >= 0) {
-        state.items[action.payload.id][index].count += 1;
-        newItems = state.items;
+    case 'ADD_PIZZA_CART': {
+      if (index !== -1) {
+        newItems[index].count += 1;
       } else {
-        const newPizza = Object.assign({}, action.payload, {
-          count: 1,
-        });
-        newItems = {
-          ...state.items,
-          [action.payload.id]: state.items[action.payload.id]
-            ? [...state.items[action.payload.id], newPizza]
-            : [newPizza],
-        };
+        newItems = [...newItems, { ...action.payload, count: 1 }];
       }
-
-      const totalCount = Object.values(newItems)
-        .flat()
-        .reduce((acc, curr) => acc + curr.count, 0);
+      const [totalCount, totalPrice] = updateState(newItems);
       return {
         ...state,
         totalCount,
-        totalPrice: state.totalPrice + action.payload.price,
+        totalPrice,
         items: newItems,
       };
+    }
+
+    case 'ADD_PIZZA_IN_CART': {
+      newItems[index].count += 1;
+      const [totalCount, totalPrice] = updateState(newItems);
+      return {
+        ...state,
+        items: newItems,
+        totalCount,
+        totalPrice,
+      };
+    }
+
+    case 'SUB_PIZZA_IN_CART': {
+      newItems[index].count =
+        newItems[index].count - 1 >= 0 ? newItems[index].count - 1 : 0;
+      const [totalCount, totalPrice] = updateState(newItems);
+      return {
+        ...state,
+        items: newItems,
+        totalCount,
+        totalPrice,
+      };
+    }
+
+    case 'DELETE_PIZZA_IN_CART': {
+      newItems = [].concat(newItems.slice(0, index), newItems.slice(index + 1));
+      const [totalCount, totalPrice] = updateState(newItems);
+      return {
+        ...state,
+        items: newItems,
+        totalCount,
+        totalPrice,
+      };
+    }
+
     default:
       return state;
   }
