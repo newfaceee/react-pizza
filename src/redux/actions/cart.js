04fed8bbd38aff25a19f4b5b1d5e1d02ac773f49
Edem <50662754+newfaceee@users.noreply.github.com>
2020-07-31 01:45:42 +0300

    Добавляет экшн для удаления пиццы из корзины

--- src/redux/actions/cart.js
+++ src/redux/actions/cart.js
@@ -2,3 +2,30 @@ export const addPizzaToCart = (pizzaObject) => ({
   type: 'ADD_PIZZA_CART',
   payload: pizzaObject,
 });
+
+export const addPizzaInCart = (id, size, doughType) => ({
+  type: 'ADD_PIZZA_IN_CART',
+  payload: {
+    id,
+    size,
+    doughType,
+  },
+});
+
+export const subPizzaInCart = (id, size, doughType) => ({
+  type: 'SUB_PIZZA_IN_CART',
+  payload: {
+    id,
+    size,
+    doughType,
+  },
+});
+
+export const deletePizzaInCart = (id, size, doughType) => ({
+  type: 'DELETE_PIZZA_IN_CART',
+  payload: {
+    id,
+    size,
+    doughType,
+  },
+});
